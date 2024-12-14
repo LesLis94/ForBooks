df4f62e0c01d3f435b40fec40f1a2b0f15aed0a1
leslis <Solncuivetru@gmail.com>
2024-12-14 19:42:19 +0300

    ntcn

---
+++ Book/Classes/Elephant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Book.Classes
+{
+	public class Elephant
+	{
+		public string Name;
+        public short EarSize;
+
+		public void	WhoAml()
+		{
+
+			Console.WriteLine($"My name is " + Name);
+			Console.WriteLine($"My ears are " + EarSize + "inches tall");
+
+		}
+	}
+}
+
--- Book/Program.cs
+++ Book/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Book.Classes;
+
+internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+
+
+
+
+        Elephants();
+        
+    }
+
+
+
+
+    public static void Elephants()
+    {
+
+        Elephant lucinda = new Elephant() { Name = "Lusinda", EarSize = 33 };
+        Elephant lloyd = new Elephant() { Name = "Lloyd", EarSize = 40 };
+
+    }
+
+}
+
+
 
