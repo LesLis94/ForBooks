c49905fd97dc16322e943380686d6265deacdd48
LesLis94 <solncuivetru@gmail.com>
2025-08-05 20:37:48 +0300

    нпнгпг

--- Book/obj/Debug/net7.0/Book.AssemblyInfo.cs
+++ Book/obj/Debug/net7.0/Book.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Book")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3c447da25b3f2b5aa42e3965b653babcb2974a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87daf6512168ec5da758f42fce9d7911971f2fc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Book")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Book")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
